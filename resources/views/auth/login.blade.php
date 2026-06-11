1eb4e0e7030b849b6dc248c9189c396e0ac539f1
Miftahul Jannah <miftahul22si@mahasiswa.pcr.ac.id>
2026-06-11 14:36:06 +0700

    final

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,10 +10,8 @@
 
     <title>SIKED MTY</title>
 
-    @vite([
-        'resources/css/app.css',
-        'resources/js/app.js'
-    ])
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+<script src="{{ asset('js/app.js') }}"></script>
 
 </head>
 
